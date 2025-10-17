fd735c32ab88cb91fdb7c8b83ac8fa80265c2470
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2025-10-17 11:31:50 -0600

    se añadieron los comentarios

--- equipo.h
+++ equipo.h
@@ -9,23 +9,28 @@ using namespace std;
 #include <string>
 #include <iomanip>
 #include <vector>
+#include <cstdlib>
 
 
-
+// Clase Equipo
 class Equipo
 {
 private:
-    string nombre;
-    int puntos;
-    int goles;
-    int golcont;
+    string nombre; // Nombre del Equipo
+    int puntos; // Puntos acumulados
+    int goles; // Goles a favor
+    int golcont; // Goles en contra
 public:
+
+    // Constructor: inicializa el equipo con su nombre y estadísticas en cero
     Equipo(string n){
         nombre = n;
         puntos = 0;
         goles = 0;
         golcont = 0;
     }
+
+    // Getters
 
     string getNombre(){
         return nombre;
@@ -43,14 +48,17 @@ public:
         return golcont;
     }
 
+    // Calcula la diferencia de goles
     int getDifGoles() const{
         return goles-golcont;
     }
 
+    // Registra el partido con los goles a favor y goles en contra
     void regPartido(int gf, int gc){
         goles += gf;
         golcont += gc;
 
+        // Se calcula el resultado y se compara para dar un resultado, agrgando puntos
         if (gf > gc) puntos += 3;  
         else if (gf == gc) puntos += 1;
     }
--- liga.h
+++ liga.h
@@ -3,31 +3,39 @@
 
 #ifndef LIGA_H
 #define LIGA_H
-#include "equipo.h"
+#include "equipo.h" // Se incluye la clase Equipo
 
 
+// Clase Liga
 
 class Liga
 {
 private:
-    vector<Equipo> equipos;
+    vector<Equipo> equipos; // Vector para agregar los equipos
 public:
 
+    // Se añade al verctor el equipo con su nombre
     void agregarEquipo(string nombre) {
         equipos.push_back(Equipo(nombre));
     }
 
+        // ==== Simular un partido ====
+    // i y j son los índices de los equipos en el vector
     void simularPartido(int i, int j) {
-        int goles1 = rand() % 5; 
+        int goles1 = rand() % 5; // goles aleatorios entre 0 y 4
         int goles2 = rand() % 5;
 
+        // Actualizar estadísticas de ambos equipos
         equipos[i].regPartido(goles1, goles2);
         equipos[j].regPartido(goles2, goles1);
 
+        // Mostrar resultado del partido
         cout << equipos[i].getNombre() << " " << goles1
              << " - " << goles2 << " " << equipos[j].getNombre() << endl;
     }
 
+
+    // Se simula todas las jornadas dependiendo el tamapo del vector
     void simularJornada() {
         for (int i = 0; i < equipos.size(); i++) {
             for (int j = i + 1; j < equipos.size(); j++) {
@@ -36,6 +44,9 @@ public:
         }
     }
 
+    // Se ordena utilizando el metodo Bubble Sort:
+    // - Primero por puntos
+    // - Luego por diferencia de goles
     void ordenarTabla() {
         for (int i = 0; i < equipos.size() - 1; i++) {
             for (int j = 0; j < equipos.size() - i - 1; j++) {
@@ -48,6 +59,7 @@ public:
         }
     }
     
+    // Muestra la tabla con todas los datos y todos los equipos
     void mostrarTabla() {
         ordenarTabla();
         cout << left << setw(15) << "Equipo"
